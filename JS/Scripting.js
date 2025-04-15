a484ba7d93d7c7a1216d9d98becfd1c083fd440c
Ghulam Mustafa <Mustafa-G2@ulster.ac.uk>
2025-04-16 00:22:49 +0100

    Making it mobile friendly and adding hamburger menu for small screens

--- JS/Scripting.js
+++ JS/Scripting.js
@@ -13,3 +13,32 @@ const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
 
+const hamburger = document.querySelector(".hamburger");
+const navList = document.getElementById("navList");
+
+hamburger.addEventListener("click", () => {
+  navList.classList.toggle("active");
+});
+
+
+// const hamburger = document.querySelector('.hamburger');
+// const navbar = document.querySelector('.navbar');
+
+// hamburger.addEventListener('click', () => {
+//   navbar.classList.toggle('active');  // Toggle navbar visibility
+//   hamburger.classList.toggle('active');  // Toggle hamburger animation
+// });
+
+// const hamburger = document.querySelector(".hamburger");
+// const navbar = docuument.querySelector(".navbar");
+
+// hamburger.addEventListener("click", ()=>{
+//     hamburger.classList.toggle("active");
+//     navbar.classList.toggle("active");
+// })
+
+
+//   function toggleMenu() {
+//     const navList = document.getElementById("navList");
+//     navList.classList.toggle("show");
+//   }
